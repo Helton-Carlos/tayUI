b66026d9a447f3c5bdb4c28c6f17d577fdccfaaa
Helton-Carlos <heltoncarlospa@gmail.com>
2022-05-26 23:17:58 -0300

    feat: enviar mundaça teste

--- src/component/Cards.vue
+++ src/component/Cards.vue
@@ -5,15 +5,15 @@ const card = cards();
 
 <template>
   <div
-    class="w-full p-4 bg-write-600 my-2 rounded shadow-lg border-2 sm:w-1/2"
+    class="w-full p-4 my-2 border-2 rounded shadow-lg bg-write-600 sm:w-1/2"
     v-for="cards in card"
     :key="cards"
   >
     <p class="font-bold">{{ cards.titulo }}</p>
     <p>{{ cards.texto }}</p>
     <router-link :to="cards.rota">
-      <button class="btn bg-indigo-600 text-white px-2 py-1 rounded-md">
-        Enviar
+      <button class="px-2 py-1 text-white bg-indigo-600 rounded-md btn">
+        Veja mais
       </button>
     </router-link>
   </div>
